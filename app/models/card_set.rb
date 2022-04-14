b6754a81eff1a8dd49fd86645dbbd800956f2f18
Evan Hildreth <me@eph.me>
2022-04-13 21:33:24 -0400

    Adding validation and tests

--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -1,4 +1,16 @@
 class CardSet < ApplicationRecord
 	validates :name, presence: true
-	validates :slug, presence: true, format: { with: /\A[0-9a-z\-]+\z/ }
+	validates :slug, presence: true, format: { with: /\A[0-9a-z\-]+\z/ }, uniqueness: true
+
+	before_validation :generate_defaults
+
+	private
+		
+	def generate_defaults
+		self.release_date ||= Time.now
+
+		return unless name && !slug
+
+		self.slug = name.downcase.gsub(/[^0-9a-z\-]+/, '-')
+	end
 end
--- test/models/card_set_test.rb
+++ test/models/card_set_test.rb
@@ -3,13 +3,35 @@ require "test_helper"
 class CardSetTest < ActiveSupport::TestCase
   test "invalid slug fails" do
     test_space = CardSet.new(name: 'Slug with space', slug: 'slug-with space')
+    assert_not test_space.save
+
     test_char = CardSet.new(name: 'Slug with special character', slug: 'slug-with-special&character')
+    assert_not test_char.save
+
     test_case = CardSet.new(name: 'Slug with uppercase', slug: 'slug-with-Uppercase')
-    assert !(test_space.save && test_char.save && test_case.save)
+    assert_not test_case.save
   end
 
   test "valid set saves" do
     test_set = CardSet.new(name: 'Test set', slug: 'test-set')
     assert test_set.save
+  end
+
+  test "autogenerate slug" do
+    test_set = CardSet.new(name: 'Everything & nothing')
+    assert test_set.save
+    assert test_set.slug == 'everything-nothing'
+  end
+
+  test "autogenerate date" do
+    test_set = CardSet.new(name: 'No date.', slug: 'no-date')
+    assert test_set.save
+    assert test_set.release_date == Date.today
+  end
+
+  test "duplicate slug fails" do
+    existing_set = CardSet.first
+    dupe_set = CardSet.new(name: 'Duplicate', slug: existing_set.slug)
+    assert_not dupe_set.save
   end
 end
