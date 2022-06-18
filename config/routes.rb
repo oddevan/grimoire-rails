cb10f0b42d1dde765db704b6d880720c10f74a94
Evan Hildreth <me@eph.me>
2022-06-18 17:29:03 -0400

    Make an all-id route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  defaults format: :json do
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    # Defines the root path route ("/")
+    # root "articles#index"
+  
+    get 'build/routes/printings', to: 'printings#all_ids'
+  end
 end
