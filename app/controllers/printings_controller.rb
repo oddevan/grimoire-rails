9c5e0beaad95f4b66bf1a9addfc29319653fd730
Evan Hildreth <me@eph.me>
2022-06-18 21:10:35 -0400

    Add list of set slugs

--- app/controllers/printings_controller.rb
+++ app/controllers/printings_controller.rb
@@ -1,6 +1,6 @@
 class PrintingsController < ApplicationController
 	def all_ids
-		ids = Printing.select "grimoire_id"
-		render json: ids.map { |p| p.grimoire_id }
+		printings = Printing.select "grimoire_id"
+		render json: printings.map { |p| p.grimoire_id }
 	end
 end
