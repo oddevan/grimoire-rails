f95b82dfed613fd1b3e7dca30feb68970f3cec77
Evan Hildreth <me@eph.me>
2022-08-14 10:52:42 -0400

    Properly show errors

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,9 +1,10 @@
 class InfoController < ApplicationApiController
 	def index
 		render json: {
-			version: 1.0,
+			version: 2.0,
 			printings: Printing.count,
 			staging: StagingPrinting.count,
+			user: current_user.nil? ? "*anonymous" : current_user.username
 		}
 	end
 end
